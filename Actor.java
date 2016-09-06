8489306bb76a3a253100401be1f3fb77b509dc1a
clbarriosb <carmenluciabarrios@hotmail.com>
2016-09-06 00:38:54 -0500

    Add files via upload

---
+++ Actor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package featurefilm;
+
+/**
+ *
+ * @author Carmen_Lucia3
+ */
+public class Actor {
+    
+}
---
+++ FeatureFilm.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package featurefilm;
+
+/**
+ *
+ * @author Carmen_Lucia3
+ */
+public class FeatureFilm {
+
+    Actor a;
+    Actor b;
+    String title;
+    String topic;
+    String category;
+    String theater; 
+    
+    public void update(Actor a, String title){
+        
+    }
+    
+    public void update(Actor a, Actor b, String title){
+        
+    }
+    
+    public void update(String topic, String title){
+        
+    }
+    
+    public boolean update(String category, String theater){
+        
+    }
+    
+    public boolean update(String title, Actor a){
+        
+    }
+    
+    public void update(Actor b, Actor a, String title){
+        
+    }
+    
+    public void update(Actor a, Actor b){
+        
+    }
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
